163bafcd5da43e88b16a6064b23a7d6404404480
Sai Panneerselvam <gqw2714@varian.com>
2020-02-23 16:36:51 -0800

    edited about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,7 +15,7 @@ return(
           <p>My name is Saigopini Panneerselvam, but I go by Sai (pronounced like "sigh"). I grew up in the Bay Area and went to college around there, double-majoring in math and computer science. It was hard being one of the only women in most of my classes but regardless,
           I kept my head up and discovered my passion for STEM during a hackathon held at my university. My friends and I created a platform that connects homeless shelters to restaurants in order feed hungry bellies sustainably by cutting down food waste.
           It was then when I realized the instant gratification that comes from computer science and the power of technology to help our fellow human beings.
-          I never did front- or backend development but in those 24 hours, I was able to contribute to something beautiful.
+          I didn't have any web development experience but in those 24 hours, I was able to contribute to something beautiful.
           My love for STEM continued to flourish by working at a healthcare company with a mission that I'm incredibly passionate about straight out of graduation.
           It was here that I stumbled into the field of data science and engineering. </p>
           <p>I try to live everyday with a growth mindset: taking everything that life throws my way as a learning opportunity which has helped me be more resilient and positive. I want to help empower other women to purse careers in STEM so that they don't face the sexism and challenges I and so many others endure. Because of my experiences, I mentor high school girls in coding and encourage them to pursue their
